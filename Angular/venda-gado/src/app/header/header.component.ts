7ed006fdadd7ad3c487d28d99bad4945f670027d
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2020-08-17 17:58:34 -0300

    Site teste

---
+++ Angular/venda-gado/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vg-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
